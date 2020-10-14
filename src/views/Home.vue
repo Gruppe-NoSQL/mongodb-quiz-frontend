7d8490b0f060be7aa61f9b819a4d40085acf9914
stat1c <fynn@core-dev.de>
2020-10-14 10:16:07 +0200

    reworked home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,37 @@
 <template>
   <div class="home">
     <v-container>
-      <v-row justify="center">
-          <v-col sm="0" md="2"></v-col>
-          <v-col sm="12" md="8" cols="12">
+      <v-row justify="center" align="center">
+          <v-col sm="12" md="6" cols="12">
             <v-card>
                 <v-card-title primary-title>
-                  <div>
-                    <h3 class="headline">Test</h3>
-                    <div>Test</div>
-                  </div>
+                  <v-container>
+                    <span class="headline">Name eingeben und Quiz starten</span>
+                  </v-container>
                 </v-card-title>
+                <v-card-text>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="6"
+                  >
+                    <v-text-field
+                      label="Vorname Nachname"
+                    ></v-text-field>
+                  </v-col>
+                </v-card-text>
+                <v-card-actions>
+                  <v-container class="mt-0">
+                    <v-btn
+                      depressed
+                      color="primary"
+                      v-on:click="startQuiz"
+                    >
+                      Quiz starten
+                    </v-btn>
+                    
+                  </v-container>
+                </v-card-actions>
             </v-card>
           </v-col>
       </v-row>
@@ -19,7 +40,28 @@
 </template>
 
 <script>
-export default {
+import { uuid } from 'vue-uuid';
 
+export default {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    startQuiz: function(){
+      let deviceId = localStorage.getItem('deviceId');
+
+      if(!deviceId){
+        deviceId = uuid.v4();
+        localStorage.setItem('deviceId', deviceId);
+      }
+
+      console.log(deviceId);
+    }
+  },
+  mounted(){
+    console.log(this.$store.state.backendServer)
+  }
 }
 </script>
