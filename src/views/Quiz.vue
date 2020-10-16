1ddb3cdb7e2cd06d0e13fd3a331597dc6f7026c8
Kebernik <maik.kebernik@sap.com>
2020-10-16 13:29:39 +0200

    Frontend Quiz.vue

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -5,15 +5,35 @@
           <v-col sm="12" md="6" cols="12">
             <v-card>
                 <v-card-title primary-title>
-                  <v-container>
-                    <span class="headline">Quiz Seite</span>
+                  <v-container class="blue darken-4" rounded>
+                   <span class="headline white--text font-weight-black text-center">NoSQL</span>
                   </v-container>
                 </v-card-title>
                 <v-card-text>
-                 
+                  <h2>Frage</h2>
+                  <p>1/6</p>
                 </v-card-text>
+                <v-divider></v-divider>
+                <br />
+                <v-radio-group>
+                  <v-radio
+                    v-for="n in 4"
+                    :key="n"
+                    :label="`Antwort ${n}`"
+                    :value="n"
+                    class="ml-4"
+                  ></v-radio>
+                </v-radio-group>
+                <br />
+                <br />
                 <v-card-actions>
-                  
+                  <v-col offset-md="9">
+                      <v-btn
+                      color="primary"
+                      elevation="3"
+                      large
+                    >WEITER</v-btn>
+                  </v-col>
                 </v-card-actions>
             </v-card>
           </v-col>
