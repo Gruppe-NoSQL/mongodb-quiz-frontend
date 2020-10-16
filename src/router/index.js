740b570b246256cdcc35f136dc28f5ab2f57298c
stat1c <fynn.weyrich@web.de>
2020-10-16 20:31:55 +0200

    replaced VueRouter with Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,49 +1,46 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Router from 'vue-router'
 import Home from '../views/Home.vue'
 import Quiz from '../views/Quiz.vue'
 import Result from '../views/Result.vue'
 import About from '../views/About.vue'
 import Feedback from '../views/Feedback.vue'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/quiz',
-    name: 'Quiz',
-    component: Quiz,
-    meta: { requiresId: true }
-  },
-  {
-    path: '/result',
-    name: 'Result',
-    component: Result,
-    meta: { requiresId: true }
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About,
-    meta: { requiresId: true }
-  },
-  {
-    path: '/feedback',
-    name: 'Feedback',
-    component: Feedback,
-    meta: { requiresId: true }
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+Vue.use(Router);
+const router = new Router({
+  routes:  [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/quiz',
+      name: 'Quiz',
+      component: Quiz,
+      meta: { requiresId: true }
+    },
+    {
+      path: '/result',
+      name: 'Result',
+      component: Result,
+      meta: { requiresId: true }
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About,
+      meta: { requiresId: true }
+    },
+    {
+      path: '/feedback',
+      name: 'Feedback',
+      component: Feedback,
+      meta: { requiresId: true }
+    }
+  ]
 });
 
 router.beforeEach((to, from, next) => {
