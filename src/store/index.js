3395f88a409d7c85506ea9161ece652412b69d80
stat1c <fynn.weyrich@web.de>
2020-10-20 15:28:56 +0200

    chaged api link

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    backendServer: 'https://api.quiz.fynn-weyrich.de'
+    backendServer: 'https://api.nosql.fynn-weyrich.de'
   },
   mutations: {
   },
